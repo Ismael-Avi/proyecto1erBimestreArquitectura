862efcb0d979ddf0d5b5295e681652d2d813f5c5
edisonogonaga <151202618+edisonogonaga@users.noreply.github.com>
2025-11-08 16:50:40 -0500

    Implement complemento2 method in Complemento class
    Added complemento2 method to calculate the two's complement of a binary number.

--- src/operaciones/Complemento.java
+++ src/operaciones/Complemento.java
@@ -3,7 +3,7 @@ package operaciones;
 public class Complemento {
     private boolean signo;
     private static final int[] numeroBits = {8, 16, 32
-                                            
+  //complemento 1                                          
  public static String complemento1(String numero) {
         if (!Conversion.esValido(numero, 0, 2)) { 
             return "Error: el número ingresado no es binario.";
@@ -20,6 +20,38 @@ public class Complemento {
         }
         return c1;
     }
-    
+
+    //complemento 2
+
+       public static String complemento2(String numero) {
+        if (!Conversion.esValido(numero, 0, 2)) {
+            return "Error: el número ingresado no es binario.";
+        }
+
+        String c2 = "";
+        boolean encontrado = false;
+
+
+        for (int i = numero.length() - 1; i >= 0; i--) {
+            char bit = numero.charAt(i);
+
+            if (encontrado) {
+                // Después del primer 1: invertir los bits
+                if (bit == '0') {
+                    c2 = '1' + c2;
+                } else {
+                    c2 = '0' + c2;
+                }
+            } else {
+                // Copiamos bits hasta encontrar el primer 1
+                c2 = bit + c2;
+                if (bit == '1') {
+                    encontrado = true; 
+                }
+            }
+        }
+
+        return c2;
+    }
     
 }
